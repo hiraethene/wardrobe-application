28c30f2935dd331de612edce55f4d935a1984efb
hiraethene <kellymariafernandes2006@gmail.com>
2025-10-29 10:25:58 +0000

    Implemented activity lifecycle methods.

--- app/src/main/java/com/example/wardrobe_application/MainActivity.java
+++ app/src/main/java/com/example/wardrobe_application/MainActivity.java
@@ -7,8 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import android.content.Context;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
+
+    //Class name for Log tag
+    //private static final String LOG_TAG = MyActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onStart() {
+        super.onStart();
+        tToast("onStart");
+    }
+
+    public void onRestart() {
+        super.onRestart();
+        tToast("onRestart");
+    }
+
+    public void onResume() {
+        super.onResume();
+        tToast("onResume");
+    }
+
+    public void onPause() {
+        super.onPause();
+        tToast("onPause");
+    }
+
+    public void onStop() {
+        super.onStop();
+        tToast("onStop");
+    }
+    public void onDestroy() {
+        super.onDestroy();
+        tToast("onDestroy");
+    }
+
+    private void tToast(String s) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, s, duration);
+        toast.show();
     }
 }
